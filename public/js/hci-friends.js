6be531d6150d2ff6a03268b0d552072ca425c800
Shiwei Zhou <s1zhou@ucsd.edu>
2018-02-07 17:13:12 -0800

    lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,7 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".name").click(function(e){
+		$(this).text(anagrammedName($(this).text()));
+		e.preventDefault();
+	})
 }
+
+
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
